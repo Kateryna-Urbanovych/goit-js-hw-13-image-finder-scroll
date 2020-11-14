133fb2099f5184af8a384da861f01a81d4ea7347
Kateryna-Urbanovych <katya_urb@ukr.net>
2020-11-14 23:51:30 +0200

    добавляю infinity scroll

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./css/styles.css";
 import getRefs from './js/get-refs';
 import imagesTpl from './templates/image-card.hbs';
 import ImagesApiSevise from './js/images-servise';
-import LoadMoreBtn from './js/load-more-btn';
 import onOpenModal from './js/modal';
 const debounce = require('lodash.debounce');
 import '@pnotify/core/dist/PNotify.css';
@@ -12,16 +11,11 @@ import pnotify from './js/pnotife-error';
 const refs = getRefs();
 
 const imagesApiSevise = new ImagesApiSevise();
-const loadMoreBtn = new LoadMoreBtn({
-    selector: 'load-more-btn',
-    hidden: true,
-});
 
 refs.searchForm.addEventListener('input', debounce(onSearchForm, 500));
-loadMoreBtn.refs.button.addEventListener('click', fetchImages);
 refs.galleryContainer.addEventListener('click', onOpenModal);
 
-let currentCoord = 0;
+infinityScroll();
 
 async function onSearchForm(event) {
     event.preventDefault();
@@ -31,7 +25,6 @@ async function onSearchForm(event) {
     try {
         if (imagesApiSevise.query === '') {
             clearImagesContainer();
-            loadMoreBtn.hide();
             return;
         }
 
@@ -44,17 +37,11 @@ async function onSearchForm(event) {
 }
 
 async function fetchImages() {
-    currentCoord = refs.galleryContainer.offsetHeight;
 
     try {
-        loadMoreBtn.show();
-        loadMoreBtn.disable();
-
         imagesApiSevise.fetchImages()
         .then(images => {
             appendImagesMarkup(images);
-            loadMoreBtn.enable();
-            scrollingPage();
             searchError(images);
         });
     } catch (error) {
@@ -70,25 +57,11 @@ function clearImagesContainer() {
     refs.galleryContainer.innerHTML = '';
 }
 
-async function scrollingPage() {
-    try {
-        window.scrollTo({
-            top: currentCoord,
-            left: 0,
-            behavior: 'smooth',
-        });
-    } catch (error) {
-        console.log(error);
-        console.log('Не удалось загрузить скроллинг страницы найденных изображений');
-    }
-}
-
 async function searchError(images) {
     try {
         const numberOfImages = images.hits.length;
         if (numberOfImages === 0) {
             pnotify.Error();
-            loadMoreBtn.hide();
         }
     } catch (error) {
         console.log(error);
@@ -96,81 +69,29 @@ async function searchError(images) {
     }
 }
 
+async function infinityScroll() {
+    try {
+        const onEntry = entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting && imagesApiSevise.query !== '') {
+                    imagesApiSevise.fetchImages()
+                    .then(images => {
+                        appendImagesMarkup(images);
+                        searchError(images);
+                    });
+                }
+            })
+        }
 
-// НА ПРОМИСАХ
-// import "./css/styles.css";
-// import getRefs from './js/get-refs';
-// import imagesTpl from './templates/image-card.hbs';
-// import ImagesApiSevise from './js/images-servise';
-// import LoadMoreBtn from './js/load-more-btn';
-// const debounce = require('lodash.debounce');
-// import '@pnotify/core/dist/PNotify.css';
-// import '@pnotify/core/dist/BrightTheme.css';
-// import pnotify from './js/pnotife-error';
+        const options = {
+            roorMargin: '200px',
+        }
 
-// const refs = getRefs();
+        const observer = new IntersectionObserver(onEntry, options);
 
-// const imagesApiSevise = new ImagesApiSevise();
-// const loadMoreBtn = new LoadMoreBtn({
-//     selector: 'load-more-btn',
-//     hidden: true,
-// });
-
-// refs.searchForm.addEventListener('input', debounce(onSearchForm, 500));
-// loadMoreBtn.refs.button.addEventListener('click', fetchImages);
-
-// let currentCoord = 0;
-
-// function onSearchForm(event) {
-//     event.preventDefault();
-
-//     imagesApiSevise.query = event.target.value;
-
-//     if (imagesApiSevise.query === '') {
-//         clearImagesContainer();
-//         loadMoreBtn.hide();
-//         return;
-//     }
-
-//     imagesApiSevise.resetPage();
-//     clearImagesContainer();
-//     fetchImages();
-// }
-
-// function fetchImages() {
-//     loadMoreBtn.show();
-//     loadMoreBtn.disable();
-
-//     currentCoord = refs.galleryContainer.offsetHeight;
-//     imagesApiSevise.fetchImages()
-//         .then(images => {
-//             appendImagesMarkup(images);
-//             loadMoreBtn.enable();
-//             scrollingPage();
-//             searchError(images);
-//         });
-// }
-
-// function appendImagesMarkup(images) {
-//     refs.galleryContainer.insertAdjacentHTML('beforeend', imagesTpl(images));
-// }
-
-// function clearImagesContainer() {
-//     refs.galleryContainer.innerHTML = '';
-// }
-
-// function scrollingPage() {
-//     window.scrollTo({
-//         top: currentCoord,
-//         left: 0,
-//         behavior: 'smooth',
-//     });
-// }
-
-// function searchError(images) {
-//     const numberOfImages = images.hits.length;
-//     if (numberOfImages === 0) {
-//         pnotify.Error();
-//         loadMoreBtn.hide();
-//     }
-// }
+        observer.observe(refs.sentinal);
+    } catch (error) {
+        console.log(error);
+        console.log("Не удалось запустить бесконечную загрузку");
+    }
+}
